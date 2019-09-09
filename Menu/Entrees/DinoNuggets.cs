651bfeeac6c96516755d952db7da58deb07e0b68
Agustin Rodriguez <agustinr@ksu.edu>
2019-09-09 17:20:44 -0500

    Aug 9/9

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DinoDIner.Menu.Entrees;
+
+namespace DinoDIner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Chicken Nugget", "Chicken Nugget", "Chicken Nugget", "Chicken Nugget", "Chicken Nugget", "Chicken Nugget" };
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 354;
+        }
+
+        public void AddNugget()
+        {
+            this.Price += .25;
+            this.Calories += 59;
+            this.Ingredients.Add("Chicken Nugget");
+        }
+    }
+}
---
+++ Menu/Entrees/SteakosaurusBurger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinerDino.Menu.Entrees
+{
+    public class SteakosaurusBurger
+    {
+        private bool bun, pickle, ketchup, mustard = true;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Steakburger Pattie" };
+                if (bun) ingredients.Add("Whole Wheat Bun");
+                if (pickle) ingredients.Add("Pickles");
+                if (ketchup) ingredients.Add("Ketchup");
+                if (mustard) ingredients.Add("Mustard");
+                return ingredients;
+            }
+        }
+
+        public SteakosaurusBurger()
+        {
+            this.Price = 5.15;
+            this.Calories = 621;
+        }
+    }
+}
