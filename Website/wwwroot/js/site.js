59361cc0cd3d0f0e4ffc8ef389c75459a1e57311
Agustin Rodriguez <agustinr@ksu.edu>
2019-11-17 17:10:27 -0600

    Website Milestone Update

--- Website/wwwroot/js/site.js
+++ Website/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+function Clear() {
+    var items = document.getElementsByClassName("MenuItems");
+    items[0].style.display = "none";
+    items[1].style.display = "none";
+    items[2].style.display = "none";
+    items[3].style.display = "none";
+}
+
+function CombosClick() {
+    Clear();
+    var Combos = document.getElementById("Combos")
+    Combos.style.display = "block";
+}
